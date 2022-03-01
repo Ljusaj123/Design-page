dc106a099aeba25220260fde8e34927f9820a915
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-03-01 15:16:28 +0100

    formatted

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const nav= document.querySelector('nav');
-const hamburger= document.querySelector('.menu-burger');
+const nav = document.querySelector("nav");
+const hamburger = document.querySelector(".menu-burger");
 
-hamburger.addEventListener('click', ()=>{
-    nav.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
 });
