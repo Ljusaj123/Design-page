513b1dfd697f9fcd48d230ff8d41ca4ef915f184
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-03-01 16:35:42 +0100

    changed class and added event listener for hamburger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const nav = document.querySelector("nav");
-const hamburger = document.querySelector(".menu-burger");
+const nav = document.querySelector(".navigation");
+const hamburger = document.querySelector(".navigation-menu");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("show");
+  hamburger.classList.toggle("is-active");
 });
